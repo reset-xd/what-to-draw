31eb11d0e3aa66c26f19bf13d92219f1de8e093b
Reset <resetwastakenwastaken@gmail.com>
2022-09-14 00:19:40 +0530

    removed auth

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from random import randint
-from re import A
 import fastapi
 from fastapi.staticfiles import StaticFiles
 from slowapi import Limiter, _rate_limit_exceeded_handler
@@ -18,48 +17,24 @@ templates = Jinja2Templates(directory="Templates")
 # region [html]
 
 @app.get("/", response_class=fastapi.responses.HTMLResponse)
-# @limiter.limit("10/minute")
 async def root(request: fastapi.Request):
-
     return templates.TemplateResponse("index.html", {"request": request, "homepage": extras.homepage_content})
 
 @app.get("/recent", response_class=fastapi.responses.HTMLResponse)
-# @limiter.limit("10/minute")
 async def root(request: fastapi.Request):
-
     return templates.TemplateResponse("index.html", {"request": request, "homepage": extras.recent_content})
 
 @app.get("/oldest", response_class=fastapi.responses.HTMLResponse)
-# @limiter.limit("10/minute")
 async def root(request: fastapi.Request):
 
     return templates.TemplateResponse("index.html", {"request": request, "homepage": extras.homepage_content})
 
 
 @app.get("/about", response_class=fastapi.responses.HTMLResponse)
-# @limiter.limit("10/minute")
 async def root(request: fastapi.Request):
     return templates.TemplateResponse("about.html", {"request": request})
 
-@app.get("/login", response_class=fastapi.responses.HTMLResponse)
-# @limiter.limit("10/minute")
-async def login_page(request: fastapi.Request):
-    return templates.TemplateResponse("login.html", {"request": request})
-
-@app.get("/profile/{username}", response_class=fastapi.responses.HTMLResponse)
-# @limiter.limit("10/minute")
-async def login_page(request: fastapi.Request, username:str):
-    data = extras.get_userinfo(username)
-    return templates.TemplateResponse("Profile.html", {"request": request, "username": data[0], "description": data[1]})
-
-
-@app.get("/signin", response_class=fastapi.responses.HTMLResponse)
-# @limiter.limit("10/minute")
-async def login_page(request: fastapi.Request):
-    return templates.TemplateResponse("signin.html", {"request": request})
-
 @app.get("/submit", response_class=fastapi.responses.HTMLResponse)
-# @limiter.limit("5/minute")
 async def login_authentication(request: fastapi.Request):
     pass
 
@@ -78,19 +53,9 @@ async def login_authentication(request: fastapi.Request, idea_id:int):
     data = extras.get_idea_by_id(idea_id)
     return templates.TemplateResponse("PerPage.html", {"request": request, "title": data[1], "description": data[2], "tag": data[3]})
 
-@app.get("/auth", response_class=fastapi.responses.JSONResponse)
-# @limiter.limit("5/minute")
-async def login_authentication(request: fastapi.Request, username:str, password:str):
-    return {"status": extras.usercheck(username, password)}
-
-@app.get("/sign-in", response_class=fastapi.responses.JSONResponse)
-# @limiter.limit("5/minute")
-async def login_authentication(request: fastapi.Request, username:str, password:str):
-    return {"status": extras.adduser(username, password)}
-
 @app.get("/submit-idea", response_class=fastapi.responses.HTMLResponse)
-# @limiter.limit("10/minute")
-async def login_authentication(request: fastapi.Request):
+@limiter.limit("1/minute")
+async def sub_idea(request: fastapi.Request):
     pass
 
 # endregion
